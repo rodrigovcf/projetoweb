f796de46b8e87ab52fa8db40afdafbb634c4025c
Rodrigo Valença <rodrigo.rvcf@gmail.com>
2022-03-02 13:16:12 -0300

    Components - Home

---
+++ Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>
+    <Main icon="home" title="Início" subtitle="Modelo Projeto Web">
+        <div className="display-4">Seja Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema de exemplo para a disciplina de Desenvolvimento Web</p>
+    </Main>
